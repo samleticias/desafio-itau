2457a75ec72a74399179953f431c7df63de51e13
samleticias <sammyaleticia09@gmail.com>
2025-03-15 17:26:00 -0300

    first commit

---
+++ src/main/java/com/example/desafioitau/DesafioitauApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioitauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioitauApplication.class, args);
+	}
+
+}
